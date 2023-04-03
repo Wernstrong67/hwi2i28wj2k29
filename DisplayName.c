f310c18589f41e5d6ca1f1d8821870301cbb75f4
Givenchy <99418994+Wernstrong67@users.noreply.github.com>
2023-04-03 15:51:40 +0100

    Create DisplayName.c

---
+++ DisplayName.c
@@ -0,0 +1,14 @@
+//to display your name n time
+#include<stdio.h>
+void main ()
+{
+  int i=1,n;
+  printf("enter the number:");
+  scanf("%d",&n);
+  do 
+  {
+    printf ("EDET NEHEMIAH ESSIEN\n");
+    i++;
+  }
+  while (i<=n);
+} 
